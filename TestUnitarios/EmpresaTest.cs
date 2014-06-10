721232563e0633e098b5594662cffbbd95f7cf72
danielbonfils <danielbonfils@hotmail.com>
2014-06-10 14:17:59 +0200

    a ver si quiere

---
+++ TestUnitarios/EmpresaTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MF0493_3;
+using MF0493_3.Models;
+
+namespace TestUnitarios
+{
+    [TestClass]
+    public class EmpresaTest
+    {
+        [TestMethod]
+        public void newTest()
+        {
+            Empresa emp = new Empresa();
+            emp.activa = true;
+            emp.email = "danielbonfils@gmail.com";
+            emp.ff = DateTime.Now;
+            emp.logo = "logo.jpg";
+            emp.usr = "admin";
+            emp.nif = "76635148E";
+            emp.telefono = "647578142";
+            emp.poblacion = "Almería";
+            emp.nombre="Mi empresa";
+
+            EmpresaManager.Nueva(emp);
+            Empresa empresa = EmpresaManager.get(emp.nif);
+            Assert.AreEqual(empresa.nombre, "Mi empresa");
+
+        }
+
+        [TestMethod]
+
+        public void ModificarTest()
+        {
+            Empresa emp = new Empresa();
+            emp.activa = true;
+            emp.email = "danielbonfils@gmail.com";
+            emp.ff = DateTime.Now;
+            emp.logo = "logo.jpg";
+            emp.usr = "admin";
+            emp.nif = "76635148E";
+            emp.telefono = "647578142";
+            emp.poblacion = "Almería";
+            emp.nombre = "Mi empresa";
+
+            EmpresaManager.Nueva(emp);
+            Empresa empresa = EmpresaManager.get(emp.nif);
+            Assert.AreEqual(empresa.nombre, "Mi empresa");
+
+            emp.nif = "76635149T";
+
+            bool editada = EmpresaManager.Modificar(emp);
+
+            Empresa empresa1 = EmpresaManager.get(emp.nif);
+
+            Assert.IsTrue(editada);
+            Assert.AreEqual(empresa1.nif, "76635149T");
+        }
+
+        [TestMethod]
+
+        public void getTest()
+        {
+            Empresa emp = new Empresa();
+            emp.activa = true;
+            emp.email = "danielbonfils@gmail.com";
+            emp.ff = DateTime.Now;
+            emp.logo = "logo.jpg";
+            emp.usr = "admin";
+            emp.nif = "76635148E";
+            emp.telefono = "647578142";
+            emp.poblacion = "Almería";
+            emp.nombre = "Mi empresa";
+
+            EmpresaManager.Nueva(emp);
+            Empresa empresa = EmpresaManager.get(emp.nif);
+            Assert.AreEqual(empresa.nombre, "Mi empresa");
+        }
+    }
+}
---
+++ TestUnitarios/UserTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MF0493_3;
+using MF0493_3.Models;
+using System.Collections.Generic;
+
+namespace TestUnitarios
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void CrearTest()
+        {
+            UserManager.CrearAdmin();
+            Usuario user = UserManager.get("admin");
+            Assert.AreEqual(user.email, "jucles@a2000.es");
+            Assert.AreEqual(user.username, "admin");
+        }
+
+        [TestMethod]
+        public void AllTest()
+        {
+            List<Usuario> lista = new List<Usuario>();
+            lista = UserManager.getAll();
+            Assert.AreEqual(lista.Count, 1);
+        }
+    }
+}
